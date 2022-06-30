7075b2b1e1a7e61485d65cb5fa827d1c085fff56
tsegaw1 <tsegisheyae@gmail.com>
2022-06-30 16:58:31 -0700

    shell preprocess

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,5 +2,9 @@
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %dbyte(s)\n", sizeof(int));
+printf("Size of an long int: %dbyte(s)\n", sizeof(long int));
+printf("Size of an long long int: %dbyte(s)\n", sizeof(long long int));
+printf("Size of an float: %dbyte(s)\n", sizeof(float));
 return (0);
 }
