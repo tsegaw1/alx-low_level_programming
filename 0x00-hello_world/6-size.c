cc04fbd1b3945695ce69483b38fb07abba398006
tsegaw1 <tsegisheyae@gmail.com>
2022-06-30 17:34:18 -0700

    shell preproces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %dbyte(s)\n", sizeof(int));
-printf("Size of an long int: %dbyte(s)\n", sizeof(long int));
-printf("Size of an long long int: %dbyte(s)\n", sizeof(long long int));
-printf("Size of an float: %dbyte(s)\n", sizeof(float));
+int c_int;
+char c_char;
+long c_long;
+long long c_long_long;
+float c_float;
+printf("Size of a char: %d byte(s)\n", sizeof(c_char));
+printf("Size of an int: %dbyte(s)\n", sizeof(c_int));
+printf("Size of an long int: %dbyte(s)\n", sizeof(c_long));
+printf("Size of an long long int: %dbyte(s)\n", sizeof(c_long_long));
+printf("Size of an float: %dbyte(s)\n", sizeof(c_float));
 return (0);
 }
