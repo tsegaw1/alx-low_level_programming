6008ac2449ee1e982f9aceb28e6d96bb067b2e09
tsegaw1 <tsegisheyae@gmail.com>
2022-07-01 15:28:17 -0700

    shell variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
+#include <studio.h>
 /**
- * *main->assign a random number to the variable n each time it is executed and print 
+ * main -> assign a random number to the varuable n each time it and print out
  * based a condition
  * Return : always 0
  */
 int main(void)
 {
 int n;
-
-srnd(time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 	printf("%d is posetive\n", n);
 if (n == 0)
-        printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 if (n < 0)
 {
-printf("%d is negative\n", n);
+	 printf("%d is negative\n", n);
 }
 return (0);
 }
