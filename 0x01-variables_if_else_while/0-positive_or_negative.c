02ee1403070a75058bc9f9cce9e18771addbcd3d
tsegaw1 <tsegisheyae@gmail.com>
2022-07-01 14:44:12 -0700

    shell variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/**
+ * *main -> assign a random number to the variable n each time it is executed and print 
+ * based a condition
+ * Return : always 0
+ */
+int main(void)
+{
+int n;
+
+srnd(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is posetive\n",n);
+if (n == 0)
+        printf("%d is zero\n",n);
+if (n < 0)
+{
+	printf("%d is negative\n",n);
+}
+return (0);
+}
