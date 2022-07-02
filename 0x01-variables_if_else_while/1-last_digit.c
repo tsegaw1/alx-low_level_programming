cc493818ec33f0f5625853a8c7c5fc40059d08fd
tsegaw1 <tsegisheyae@gmail.com>
2022-07-02 06:56:48 -0700

    shell variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,12 +12,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-	printf("%d is posetive\n", n);
+{
+	printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+}
 if (n == 0)
-	printf("%d is zero\n", n);
+{
+       printf("Last digit of %d is 0 and is 0\n",
+                      n);
+}
 if (n < 0)
 {
-	 printf("%d is negative\n", n);
+	 printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			 n, n % 10);
 }
 return (0);
 }
