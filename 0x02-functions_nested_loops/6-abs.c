133e35be4c4323e95dab43a4d8aa35d9713d2dbf
tsegaw1 <tsegisheyae@gmail.com>
2022-07-05 12:01:11 -0700

    shell function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  _abs -> return absolute value of a number
+ *  @n: parameter
+ *  Return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (int n)
+		n = -n;
+	return (n);
+}
