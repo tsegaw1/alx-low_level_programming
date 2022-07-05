afbaa41ee9c0de2127c190e4ef63ab7e0031b8ec
tsegaw1 <tsegisheyae@gmail.com>
2022-07-05 10:49:40 -0700

    shell function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 -> prints the lowercase alphabets
+ * print_alphabet_x10(void);
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int j;
 	int x;
-
-	for (x = 0; x < 10; x++)
-	{
+	
+	for (x = 0; x < 10; x ++)
+	{ 
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
-		putchar ('\n');
+		_putchar(\n);
 	}
 }
