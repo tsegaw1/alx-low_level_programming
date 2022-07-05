d1adcb51a1269b037c1439e2d87833a95b334559
tsegaw1 <tsegisheyae@gmail.com>
2022-07-05 09:35:32 -0700

    shell function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print _alphabet -> lowercase alphabets
+ */
+void print _alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j ++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
