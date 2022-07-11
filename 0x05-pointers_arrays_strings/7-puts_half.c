5c1b6215b46b714b69829ce01b7c72c4e9cec0e4
tsegaw1 <tsegisheyae@gmail.com>
2022-07-11 13:30:27 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - prints last half of a string
+ *
+ * @str:input
+ *
+ */
+void puts_half(char *str)
+{
+	int i, length, half;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+		for (i = half; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half = (length - 1) / 2;
+		for (i = half + 1; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
