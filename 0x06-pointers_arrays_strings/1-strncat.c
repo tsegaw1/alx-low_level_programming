78c06de97bff0912c01fb6c1483003cba9b7b005
tsegaw1 <tsegisheyae@gmail.com>
2022-07-15 15:34:53 -0700

    mor pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat -> function to append some charx
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * return: string
+ */
+char *_strncat(char *dest, char *stc, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
