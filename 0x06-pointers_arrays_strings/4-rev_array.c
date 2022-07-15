0206ef8b83e4468266cae2b788c4b6e34c0d740f
tsegaw1 <tsegisheyae@gmail.com>
2022-07-15 16:18:56 -0700

    more pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,10 +7,8 @@
 void reverse_array(int *a, int n)
 {
 	int *p, i, aux, k;
-	a = [1, 2, 3, 4];
-	aux = 1
-		a=[4, 2, 3, 1]
-		p = a;
+	
+	p = a;
 	for (i = 0; i < n; i++)
 		p++;
 	for (k = 0; k < n / 2; k++)
