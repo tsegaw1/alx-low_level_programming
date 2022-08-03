646454c2ac7399106c5d7ed2cc07f5000aff972c
tsegaw1 <tsegisheyae@gmail.com>
2022-08-03 16:23:25 -0700

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - a function to print a name
+ *
+ * @name: name variable pointer
+ * @f: a function pointer
+ * Return: returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
