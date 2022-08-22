cad7abcf1ca81341a5367d6420a741c6d3b60515
tsegaw1 <tsegisheyae@gmail.com>
2022-08-22 15:03:41 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,25 +12,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int fd, rstatus, i;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
-	write(file, text_content, i);
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++);
+		rstatus = write(fd, text_content, i);
+		if (rstatus == -1)
+			return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
